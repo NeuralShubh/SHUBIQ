63a9f42bedbc636c71d6e07cabc42679f5f2efb8
Shubham Patil <shubham95792@gmail.com>
2026-04-01 11:05:56 +0530

    Update section divider to full-width line

--- src/app/components/SectionDivider.tsx
+++ src/app/components/SectionDivider.tsx
@@ -5,10 +5,10 @@ import ScrollReveal from "./ScrollReveal"
 export default function SectionDivider() {
   return (
     <ScrollReveal>
-      <div className="flex items-center justify-center py-12 md:py-16">
-        <div className="w-12 h-[1px] bg-[rgb(var(--cream-rgb)/0.2)]" />
-        <div className="w-2 h-2 rounded-full border border-[rgb(var(--cream-rgb)/0.35)] mx-4" />
-        <div className="w-12 h-[1px] bg-[rgb(var(--cream-rgb)/0.2)]" />
+      <div className="py-12 md:py-16">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+          <div className="h-px w-full bg-[rgb(var(--gold-rgb)/0.18)]" />
+        </div>
       </div>
     </ScrollReveal>
   )
