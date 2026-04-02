f46768de68e8eb725c323df47c50de4cf85a8377
Shubham Patil <shubiq@srv1469158.hstgr.cloud>
2026-04-02 09:28:13 +0000

    Reduce founder divider vertical spacing to tighten section gaps

--- src/app/components/SectionDivider.tsx
+++ src/app/components/SectionDivider.tsx
@@ -5,7 +5,7 @@ import ScrollReveal from "./ScrollReveal"
 export default function SectionDivider() {
   return (
     <ScrollReveal>
-      <div className="py-12 md:py-16">
+      <div className="py-5 md:py-7">
         <div className="max-w-6xl mx-auto px-4 sm:px-6">
           <div className="h-px w-full bg-[rgb(var(--gold-rgb)/0.18)]" />
         </div>
